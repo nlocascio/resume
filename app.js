ad46b45aae5d9bd86cadeb3812526c260baa9b2b
Nick LoCascio <nlocascio@gmail.com>
2016-08-24 09:52:51 -0400

    Not using these components yet

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 window.Vue = require('vue');
 
-// Vue.component('welcome', require('./components/Welcome.vue'));
-// Vue.component('about', require('./components/About.vue'));
 Vue.component('resume', require('./components/Resume.vue'));
-// Vue.component('portfolio', require('./components/Portfolio.vue'));
 
 var app = new Vue({
     el: 'body'
