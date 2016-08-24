8334c893cc5b646b56b117ee00b6a32ba55289e9
Nick LoCascio <nlocascio@gmail.com>
2016-08-24 10:02:08 -0400

    Folder restructuring

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-vue');
 
 elixir((mix) => {
-    mix.less('./app.less')
-        .copy('node_modules/font-awesome/fonts', 'public/fonts')
-        .webpack('./app.js')
+    mix.less('./app.less', 'public/dist/css')
+        .copy('node_modules/font-awesome/fonts', 'public/dist/fonts')
+        .webpack('./app.js', 'public/dist/js')
         .browserSync({
             proxy: 'resume.dev'
         });
